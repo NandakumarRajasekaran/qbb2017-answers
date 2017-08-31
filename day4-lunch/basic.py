131a361ade7359fb0f910c64d1396c7b95f83e82
NandakumarRajasekaran <rnanda.chennai@gmail.com>
2017-08-31 14:01:54 -0400

    Update

--- day4-lunch/basic.py
+++ day4-lunch/basic.py
@@ -2,7 +2,7 @@
 
 
 """
-usage: ./basic.py <file1.ctab> <file2.ctab> 
+usage: ./basic.py <file1.ctab> <file2.ctab> <output name>
 """
 import sys
 import pandas as pd
@@ -22,21 +22,26 @@ df3=pd.merge(df1,df2,on=["t_name"],how="inner")
 roi=df3!=0 
 #=df3["FPKM_y"]!=0
 #roi=roi1 and roi2
-df3=df3[~(df3==0).any(axis=1)]
+#df3=df3[~(df3==0).any(axis=1)]
+#print ~(df3==0).any(axis=1)
 #print df3.head()
+df3["FPKM_x"]=df3["FPKM_x"]+1
+df3["FPKM_y"]=df3["FPKM_y"]+1
+print df3.head()
 df3["FPKM_x"]=np.log(df3["FPKM_x"])
 df3["FPKM_y"]=np.log(df3["FPKM_y"])
 
 plt.figure()
-plt.scatter(df3["FPKM_x"],df3["FPKM_y"],alpha=0.3,s=10)
+plt.scatter(df3["FPKM_x"],df3["FPKM_y"],alpha=0.3,s=2)
 plt.title("FPKM Comparison of \n 893 and 915",alpha=0.3)
-plt.legend(loc='upper left')
+#plt.legend(loc='upper left')
 plt.xlabel("893 FPKM")
 plt.ylabel("915 FPKM")
 
-fit = np.polyfit(x=df3["FPKM_x"],y=df3["FPKM_y"],deg=1)
-plt.plot(df3["FPKM_x"],fit[0]*df3["FPKM_x"]+fit[1],'-')
-print fit
+#fit = np.polyfit(x=df3["FPKM_x"],y=df3["FPKM_y"],deg=1)
+#plt.plot(df3["FPKM_x"],fit[0]*df3["FPKM_x"]+fit[1],'-')
+plt.plot(np.unique(df3["FPKM_x"]), np.poly1d(np.polyfit(df3["FPKM_x"], df3["FPKM_y"], 1))(np.unique(df3["FPKM_x"])))
+#print fit
 plt.savefig(sys.argv[3])
 plt.close()
 
