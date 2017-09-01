fcd8f38b71fcdff6dd1f428b27263c91a4f76302
NandakumarRajasekaran <rnanda.chennai@gmail.com>
2017-09-01 17:01:32 -0400

    Sorted files output

--- day5-lunch/question4.py
+++ day5-lunch/question4.py
@@ -13,23 +13,37 @@ import pandas as pd
 
 #df=pd.DataFrame()
 colno=5
-df1=pd.read_csv(sys.argv[2],header=None,sep="\t").values
+df1=pd.read_csv(sys.argv[2],header=None,sep="\t")
+df1=df1.sort_values(df1.columns[0]).values
 df1=list(map(float,df1[:,colno]))
-#print df1
-df2=pd.read_csv(sys.argv[3],header=None,sep="\t").values
+
+df2=pd.read_csv(sys.argv[3],header=None,sep="\t")
+df2=df2.sort_values(df2.columns[0]).values
 df2=list(map(float,df2[:,colno]))
-df3=pd.read_csv(sys.argv[4],header=None,sep="\t").values
+df3=pd.read_csv(sys.argv[4],header=None,sep="\t")
+df3=df3.sort_values(df3.columns[0]).values
+
 df3=list(map(float,df3[:,colno]))
-df4=pd.read_csv(sys.argv[5],header=None,sep="\t").values
+df4=pd.read_csv(sys.argv[5],header=None,sep="\t")
+df4=df4.sort_values(df4.columns[0]).values
 df4=list(map(float,df4[:,colno]))
 
 df=pd.read_csv(sys.argv[1],header=None,sep="\t")
+df=df.sort_values(df.columns[5])
+#print df
 
-df=df.values
-df=df[1:,11]
+n,p=df.shape
+#print df.values[n-1,:]
+df=df.values[0:n-1,:]
+print df.shape
+#print df
+df=df[:,11]
+
+
 df=np.asarray(list(map(float,df)))
 
 df5=np.column_stack((df1,df2,df3,df4))
+
 
 #print df5
 
