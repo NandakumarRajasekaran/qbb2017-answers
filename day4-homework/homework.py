15a6f618e0b2b3e20b3d9e8005035ed9279ef8e6
NandakumarRajasekaran <rnanda.chennai@gmail.com>
2017-08-31 21:42:05 -0400

    Update

--- day4-homework/homework.py
+++ day4-homework/homework.py
@@ -40,7 +40,7 @@ for sample in df_samples["sample"][soi]:
     fpkms_male.append((df[roi]["FPKM"]).values)
 df_rep=pd.read_csv(sys.argv[3]) 
  
-fpkms_frep=[0,0,0,0]
+fpkms_frep=[None,None,None,None]
 soi=df_rep["sex"]=="female"
 
 for sample in df_rep["sample"][soi]:
@@ -50,7 +50,7 @@ for sample in df_rep["sample"][soi]:
     roi=df["t_name"]==transcript
     fpkms_frep.append((df[roi]["FPKM"]).values)
 
-fpkms_mrep=[0,0,0,0]
+fpkms_mrep=[None,None,None,None]
 soi=df_rep["sex"]=="male"
 
 for sample in df_rep["sample"][soi]:
@@ -64,13 +64,18 @@ for sample in df_rep["sample"][soi]:
 
 plt.figure()
 x=[0,1,2,3,4,5,6,7]
+plt.rc('xtick',labelsize=20)
+plt.rc('ytick',labelsize=20)
 plt.plot(fpkms,c="red",linewidth=2)
 plt.plot(fpkms_male,c='blue',linewidth=2)
-plt.plot(fpkms_frep,'o',c="red")
-plt.plot(fpkms_mrep,'o',c="blue")
-plt.legend(['female','male'],loc="upper center",frameon=False)
-plt.xticks(x,["10","11","12","13","14A","14B","14C","14D"])
-plt.xlabel("developmental stage")
-plt.ylabel("FPKM")
-plt.savefig("timecourse.png")
+plt.plot(fpkms_frep,'o',c="red",markersize=10)
+plt.plot(fpkms_mrep,'o',c="blue",markersize=10)
+plt.title("FBtr0331261",size=20)
+plt.legend(['female','male','female replicates','male replicates'],bbox_to_anchor=(1.5,0.6),frameon=False,numpoints=1)
+plt.axis(linewidth=20,size=20)
+plt.xticks(x,["10","11","12","13","14A","14B","14C","14D"],rotation=90)
+plt.tick_params(right='off',top='off',direction='out')
+plt.xlabel("developmental stage",size=20)
+plt.ylabel("FPKM",size=20)
+plt.savefig("timecourse.png",bbox_inches='tight')
 plt.close
