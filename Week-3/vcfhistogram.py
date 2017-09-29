c1fe5f7d65c6963fe85de1c8be9d071ec311ddb4
NandakumarRajasekaran <rnanda.chennai@gmail.com>
2017-09-29 10:11:35 -0400

    Week3

---
+++ Week-3/vcfhistogram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib.pyplot as plt
+
+f=open(sys.argv[1])
+
+allele_freq=[]
+for line in f:
+    if line.startswith("#"):
+        continue
+    line=line.rstrip("\t\n").split()
+    
+    af=line[7].split(";")
+    
+    af1=af[3][3:]
+    if "," in af1:
+        af1=af1.split(",")
+        for i in af1:
+            allele_freq.append(float(i))
+    else:
+        allele_freq.append(float(af1))
+    
+plt.figure()
+plt.hist(allele_freq,bins=20)
+plt.savefig(sys.argv[2])    
+plt.close()    
+    
