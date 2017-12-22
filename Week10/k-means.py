695dea54f0113e946ff37dd939c655a798356cf8
NandakumarRajasekaran <rnanda.chennai@gmail.com>
2017-12-22 13:51:48 -0500

    update

--- Week10/k-means.py
+++ Week10/k-means.py
@@ -13,12 +13,18 @@ data=df[coi].values
 data_new=vq.whiten(data)
 codebook,distortion=vq.kmeans(vq.whiten(data),5) #the top cluster in heat map is not very clear. I used approximately 2 or 5 clusters
 code,dist=vq.vq(data_new,codebook)
+print code
+colors=np.asarray(['g','r','y','b','m'])
+colorcode=colors[code]
+print colorcode
 #print codebook
 np.save('code.npy',code)
 
+
+
 plt.figure()
 
-plt.scatter(data_new[:,0],data_new[:,1],color='b')
-plt.scatter(codebook[:,0],codebook[:,1],color='r')
+plt.scatter(data_new[:,0],data_new[:,1],c=np.ndarray.tolist(colorcode))
+plt.scatter(codebook[:,0],codebook[:,1],color='k')
 plt.savefig('kcluster5.png')
 plt.close()
